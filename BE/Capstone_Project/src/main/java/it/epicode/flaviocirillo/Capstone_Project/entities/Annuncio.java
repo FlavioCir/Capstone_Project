677ce7b5162ed6c02465c4b6f08028b1e61b7c6b
Flavio Cirillo <scatto2003@gmail.com>
2023-04-11 15:52:52 +0200

    Capstone_project

--- BE/Capstone_Project/src/main/java/it/epicode/flaviocirillo/Capstone_Project/entities/Annuncio.java
+++ BE/Capstone_Project/src/main/java/it/epicode/flaviocirillo/Capstone_Project/entities/Annuncio.java
@@ -2,6 +2,7 @@ package it.epicode.flaviocirillo.Capstone_Project.entities;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -54,6 +55,8 @@ public class Annuncio {
 	private String immatricolazione;
 	private String localita;
 	private double prezzo;
+	
+	@Column(length = 1000)
 	private String descrizione;
 	
 	 @ManyToOne
